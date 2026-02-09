7bb42e3a52b075f578c96ef04db3454d2c26592e
Jan Freitag <jan.freitag@htl-braunau>
2026-02-09 09:00:17 +0100

    Initial Commit

---
+++ Exams-App/Exams_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exams_AppApp.swift
+//  Exams-App
+//
+//  Created by 187hahaxD on 09.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Exams_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
